53e042975e0f69c92fe23dbe2d3c1a37d7afb904
Ian <ianpc122@gmail.com>
2020-01-28 10:10:08 +0000

    updated css

--- controller.rb
+++ controller.rb
@@ -4,15 +4,15 @@ require('sinatra/contrib/all')
 require_relative('./models/game.rb')
 also_reload('./models/*')
 
-get '/start/:hand1/:hand2' do
-  @player = "#{Game.start(params[:hand1], params[:hand2])}"
-  erb(:result)
-end
-
 get '/' do
   erb(:home)
 end
 
 get '/about_us' do
   erb(:about_us)
+end
+
+get '/start/:hand1/:hand2' do
+  @player = "#{Game.start(params[:hand1], params[:hand2])}"
+  erb(:result)
 end
